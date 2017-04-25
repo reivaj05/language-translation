5dedfcdf56ccbb828b477023535b9200dd62ee9d
Javier Adriel Sanchez <reivaj0705@gmail.com>
2017-04-25 15:15:42 -0500

    decoding_layer_infer

--- helper.py
+++ helper.py
@@ -2,6 +2,7 @@ import os
 import pickle
 import copy
 import numpy as np
+import io
 
 
 CODES = {'<PAD>': 0, '<EOS>': 1, '<UNK>': 2, '<GO>': 3 }
@@ -12,7 +13,7 @@ def load_data(path):
     Load Dataset from File
     """
     input_file = os.path.join(path)
-    with open(input_file, 'r', encoding='utf-8') as f:
+    with io.open(input_file, 'r', encoding='utf-8') as f:
         data = f.read()
 
     return data
